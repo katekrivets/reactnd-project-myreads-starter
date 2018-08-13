8ab80ddf3224431a64fbf6cc8aae2cb174736540
katerren <katerren@gmail.com>
2018-08-13 18:26:50 +0300

    upd books with no image issue / dropdown in search upd

--- src/components/BooksGrid.js
+++ src/components/BooksGrid.js
@@ -4,14 +4,13 @@ import ShelfDropdown from './ShelfDropdown'
 class BooksGrid extends Component {
     render() {
         const {books, changeShelf} = this.props
-        console.log("books",books)
         return (
             <ol className="books-grid">
             {books.map((book) => (
                 <li key={book.id}>
                     <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks.thumbnail}")` }}></div>
+                        <div className="book-cover" alt={`${book.title}`} style={{ width: 128, height: 193, backgroundImage: `url("${book.imageLinks?book.imageLinks.thumbnail:''}")` }}></div>
                         <ShelfDropdown 
                         book={ book }
                         books={ books }
